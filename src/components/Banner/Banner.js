59c535ef93712dcb895920310cfab88705e9690b
Phil <phil@pcollins.tech>
2019-09-30 12:02:00 +0100

    removed blank pages to fix build break

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,26 +1,33 @@
 import React from "react"
 import { Carousel } from "react-bootstrap"
 
-const Banner = ({ imageOne, imageTwo, imageThree }) => {
+const Banner = ({
+  imageOne,
+  imageTwo,
+  imageThree,
+  titleOne,
+  titleTwo,
+  titleThree,
+}) => {
   return (
     <div>
       <Carousel>
         <Carousel.Item>
           <img className="d-block w-100" src={imageOne} alt="First slide" />
           <Carousel.Caption>
-            <h2>Health Food With a Difference</h2>
+            <h2>{titleOne}</h2>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img className="d-block w-100" src={imageTwo} alt="First slide" />
           <Carousel.Caption>
-            <h2>Second slide label</h2>
+            <h2>{titleTwo}</h2>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img className="d-block w-100" src={imageThree} alt="First slide" />
           <Carousel.Caption>
-            <h2>Third slide label</h2>
+            <h2>{titleThree}</h2>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
--- src/pages/our-brands.js
+++ src/pages/our-brands.js
@@ -20,7 +20,7 @@ const OurBrandsPage = () => (
           Our Products include highly nutritious, milled and crunchy seed
           toppings, delicious wholesome innovative snacks, low sugar cereals and
           gluten free baking mixes. We weave the valuable properties of seeds
-          and cereals such as flaxseed, sunflower seed, pumpkinseed and chia,
+          and cereals such as flaxseed, sunflower seed, pumpkinseed and chia ,
           oats, quinoa and amaranth into delicious products using technical
           excellence and a dash of ingenuity to produce products that delight
           our customers.
