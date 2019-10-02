044a02bfc9aa02e04c0416e6c2ca7c50843861d2
Phil <phil@pcollins.tech>
2019-10-02 13:46:10 +0100

    Our Promise Page

---
+++ src/components/HomeCompostablePackaging/HomeCompostablePackaging.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { Container, Row } from "react-bootstrap"
+import { Link } from "gatsby"
+import homeCompostablePackaging from "../../assets/images/homeCompostablePackaging.png"
+
+const HomeCompostablePackaging = () => {
+  return (
+    <Container>
+      <Row className="d-flex justify-content-between">
+        <div className="col-sm-6 pt-2">
+          <h2 style={{ color: "white" }}>
+            Home <span className="green">Compostable</span> Packaging
+          </h2>
+          <p>
+            We are delighted to introduce our plastic free Home Compostable
+            Packaging. It is 100% biodegradable and breaks down naturally and
+            easily in the environment.
+          </p>
+          <p>
+            We can now benefit from flaxseed super nutrients - omega 3, gentle
+            fibre, protein and minerals which also caring for the earth.
+            Introducing this natural cellulose pack is part of out commitment to
+            a sustainable future for us and our planet.
+          </p>
+          <p>Thank you for choosing the option that leaves no trace.</p>
+        </div>
+        <div className="col-sm-6">
+          <img src={homeCompostablePackaging} alt="" />
+        </div>
+      </Row>
+    </Container>
+  )
+}
+
+export default HomeCompostablePackaging
---
+++ src/pages/our-promise.js
@@ -0,0 +1,119 @@
+import React from "react"
+import Layout from "../layout/Layout"
+import SEO from "../components/seo"
+import Banner from "../components/Banner"
+import { Container, Row } from "react-bootstrap"
+import HomeCompostablePackaging from "../components/HomeCompostablePackaging"
+import ContactUs from "../components/ContactUs"
+
+import banner from "../assets/images/ourPromiseBanner.png"
+import blankLabel from "../assets/images/blackLabel.png"
+
+class OurPromisePage extends React.Component {
+  render() {
+    console.log("POSTS", this.props)
+    return (
+      <Layout>
+        <SEO title="Home" />
+        <Banner img={banner} title="Private Label" />
+        <Container className="text-center my-5 private_label">
+          <Row>
+            <div className="col-sm-6">
+              <img src={blankLabel} alt="Blank Label" />
+            </div>
+            <div className="col-sm-6">
+              <p>
+                Virginia Health Food Ltd creates healthy natural products, grows
+                more ingredients locally and supports a nature education
+                community through Origin Green, Ireland’s food and drink
+                sustainability programme.
+              </p>
+              <p>
+                We endeavour to partner with only those who have a verified
+                commitment to sustainability.
+              </p>
+              <p>
+                This programme brings together our food industry – from farmers
+                to food producers, retailers to foodservice operators – with the
+                common goal of sustainable food production.
+              </p>
+              <h2 className="py-3">Quality Products</h2>
+              <p>
+                The overall ambition of the Origin Green programme is that farms
+                and food manufacturing businesses throughout Ireland sign up to
+                the sustainability agenda, making measurable commitments to
+                producing in a sustainable manner, with progress independently
+                assessed and verified.
+              </p>
+              <p>
+                We, Virginia Health Food Ltd, are committed to working with
+                Origin Green to improve sustainable thinking through
+                collaboration in this sector.
+              </p>
+            </div>
+          </Row>
+        </Container>
+
+        <div id="qualityAndSustainability">
+          <HomeCompostablePackaging />
+        </div>
+        <Container>
+          <Row>
+            <h2 className="pt-3">Raw Materials</h2>
+            <p>
+              The overall ambition of the Origin Green programme is that farms
+              and food manufacturing businesses throughout Ireland sign up to
+              the sustainability agenda, making measurable commitments to
+              producing in a sustainable manner, with progress independently
+              assessed and verified.
+            </p>
+            <p>
+              We, Virginia Health Food Ltd, are committed to working with Origin
+              Green to improve sustainable thinking through collaboration in
+              this sector.
+            </p>
+          </Row>
+        </Container>
+        <Container>
+          <Row>
+            <h2 className="pt-3">Waste and Recycling</h2>
+            <p>
+              The overall ambition of the Origin Green programme is that farms
+              and food manufacturing businesses throughout Ireland sign up to
+              the sustainability agenda, making measurable commitments to
+              producing in a sustainable manner, with progress independently
+              assessed and verified.
+            </p>
+            <p>
+              We, Virginia Health Food Ltd, are committed to working with Origin
+              Green to improve sustainable thinking through collaboration in
+              this sector.
+            </p>
+          </Row>
+        </Container>
+        <Container>
+          <Row>
+            <h2 className="pt-3">Communication</h2>
+            <p>
+              The overall ambition of the Origin Green programme is that farms
+              and food manufacturing businesses throughout Ireland sign up to
+              the sustainability agenda, making measurable commitments to
+              producing in a sustainable manner, with progress independently
+              assessed and verified.
+            </p>
+            <p>
+              We, Virginia Health Food Ltd, are committed to working with Origin
+              Green to improve sustainable thinking through collaboration in
+              this sector.
+            </p>
+          </Row>
+        </Container>
+        <div id="contactUs">
+          <ContactUs />
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default OurPromisePage
