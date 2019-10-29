65172959d1ca07ea5c6475e65763e65f88354906
Phil <phil@pcollins.tech>
2019-10-29 15:55:15 +0000

    added context to gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,9 @@
  */
 
 // You can delete this file if you're not using it
+
+import React from "react"
+import EnquiryState from "./src/context/enquiry/EnquiryState"
+export const wrapRootElement = ({ element }) => (
+  <EnquiryState>{element}</EnquiryState>
+)
